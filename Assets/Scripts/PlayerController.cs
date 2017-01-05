b26d6328bad95a02d90bdbdeaaa1bd40dfd83bad
maiger <mage_ege@hotmail.com>
2017-01-05 19:04:11 +0200

    Added character animations
    Added character animations for idle, walk, jump and throw.
    Throwing is not implemented yet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,14 +20,18 @@ public class PlayerController : MonoBehaviour {
 
     public bool isGrounded;
 
+    private Animator anim;
+
 	// Use this for initialization
 	void Start () {
         theRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        // TODO: This could be done with raycasting, but would it be better?
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
 
 		if(Input.GetKey(left))
@@ -46,5 +50,18 @@ public class PlayerController : MonoBehaviour {
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
         }
+
+        // Flip character based on movement direction
+        if(theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if(theRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Give updated parameter values to animator
+        anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
     }
 }
