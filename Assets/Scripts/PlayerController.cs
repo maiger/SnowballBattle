2231176d033243d6937bfba945ccef345844a7dd
maiger <mage_ege@hotmail.com>
2017-01-05 16:49:41 +0200

    Added a playercController
    Player can now move and jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwBall;
+
+    private Rigidbody2D theRB;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+		if(Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        } else if(Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        } else
+        {
+            // TODO: Add surface based sliding
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if(Input.GetKeyDown(jump) &&  isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+    }
+}
