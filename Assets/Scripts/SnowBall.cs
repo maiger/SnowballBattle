b32e87c1ee6acfde63f9c7629dd8b7001881ebc4
maiger <mage_ege@hotmail.com>
2017-01-06 19:25:12 +0200

    Added a second character
    Added a second character that can be controlled with arrow keys and
    enter to throw
    Updated animations and graphics for the second character
    Fix: snowballs are not destroyed anymore if thrown during a running jump

--- Assets/Scripts/SnowBall.cs
+++ Assets/Scripts/SnowBall.cs
@@ -20,8 +20,6 @@ public class SnowBall : MonoBehaviour {
         theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
 	}
 
-    // FIX: Bug, if character is jumping and moving sideways, snowball might collide with the character immediately
-    // Move the spawnpooint further away from the player, or check if the snowball collided with the character who spawned the snowball.
     void OnTriggerEnter2D(Collider2D other)
     {
         Instantiate(snowBallEffect, transform.position, transform.rotation);
